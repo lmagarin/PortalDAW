5a612104b4491fe458d32f01eb800ba97f4163cd
rafael miranda <rafael miranda>
2016-01-12 13:45:14 +0100

    Commit 12-01-16 v.4

--- ejerciciosCliente/ejercicios/Tema5/FormularioCompleto/js/javascript.js
+++ ejerciciosCliente/ejercicios/Tema5/FormularioCompleto/js/javascript.js
@@ -41,17 +41,11 @@ window.addEventListener("load", function(){
     }
 
     function validateName(string){
-        var RegExName = /^([a-z ñáéíóú]{1,60})$/i;
-        if(!RegExName.test(string))
-            return false;
-        return true;
+        return /^([a-z ñáéíóú]{1,60})$/i.test(string);
     }
 
     function validateSurname(string){
-        var RegExSurname = /^([a-z ñáéíóú -']+)$/i;
-        if(!RegExSurname.test(string))
-            return false;
-        return true;
+        return /^([a-z ñáéíóú -']+)$/i.test(string);
     }
 
     function validateGender(){
@@ -75,24 +69,15 @@ window.addEventListener("load", function(){
     }
 
     function validateWeight(string){
-        var RegExWeight = /^[0-9]{1,3}$/;
-        if(!RegExWeight.test(string))
-            return false;
-        return true;
+        return /^[0-9]{1,3}$/.test(string);
     }
 
     function validateMail(string){
-        var RegExMail = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i;
-        if(!RegExMail.test(string))
-            return false;
-        return true;
+        return /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i.test(string);
     }
 
     function validateDNI(string){
-        var RegExDNI = /^[0-9]{8}[A-Z]$/i;
-        if(!RegExDNI.test(string))
-            return false;
-        return true;
+        return /^[0-9]{8}[A-Z]$/i.test(string);
     }
 
     function validateDate(string){
@@ -111,64 +96,75 @@ window.addEventListener("load", function(){
     }
 
     function validatePhonenumber(string){
-        var RegExPhone = /^[0-9]{9}$/i;
-        if(!RegExPhone.test(string))
-            return false;
-        return true;
+        return /^[0-9]{9}$/i.test(string);
     }
 
     function validateBankAccount(string){
-        var RegExBankAccount = /^[0-9]{4}[-][0-9]{4}[-][0-9]{2}[-][0-9]{10}$/i;
-        if(!RegExBankAccount.test(string))
-            return false;
-        return true;
+        return /^[0-9]{4}[-][0-9]{4}[-][0-9]{2}[-][0-9]{10}$/i.test(string);
     }
 
     function validateUrl(string){
-        var RegExUrl = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \?=.-]*)*\/?$/i;
-        if(!RegExUrl.test(string))
-            return false;
-        return true;
+        return /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \?=.-]*)*\/?$/i.test(string);
     }
 
     function completeValidation(){
-        if(isEmptyField(name.value) || !validateName(name.value))
-            nameError.innerHTML = finalErrorMessage;
-            name.focus();
-        if(isEmptyField(surname.value) || !validateSurname(surname.value))
-            surnameError.innerHTML = finalErrorMessage;
-            surname.focus();
-        if(!validateGender(gender.value))
-            genderError.innerHTML = finalErrorMessage;
-            genderMale.focus();
-        if(!validateIdiom(idiom.value))
-            idiomError.innerHTML = finalErrorMessage;
-            idiomCastellano.focus();
-        if(isEmptyField(weight.value) || !validateWeight(weight.value))
-            weightError.innerHTML = finalErrorMessage;
-            weight.focus();
-        if(isEmptyField(mail.value) || !validateMail(mail.value))
-            mailError.innerHTML = finalErrorMessage;
-            mail.focus();
-        if(isEmptyField(dni.value) || !validateDNI(dni.value))
-            dniError.innerHTML = finalErrorMessage;
-            dni.focus();
-        if(isEmptyField(birthdate.value) || !validateDate(birthdate.value))
-            birthdateError.innerHTML = finalErrorMessage;
-            birthdate.focus();
-        if(isEmptyField(phone.value) || !validatePhonenumber(phone.value))
-            phoneError.innerHTML = finalErrorMessage;
-            phone.focus();
-        if(isEmptyField(bankAccount.value) || !validateBankAccount(bankAccount.value))
-            accountError.innerHTML = finalErrorMessage;
-            bankAccount.focus();
-        if(isEmptyField(url.value) || !validateUrl(url.value))
+    	var focusingElement = undefined;
+    	if(isEmptyField(url.value) || !validateUrl(url.value)){
             urlError.innerHTML = finalErrorMessage;
-            url.focus();
+            focusingElement = url;
+        }
+        if(isEmptyField(bankAccount.value) || !validateBankAccount(bankAccount.value)){
+            accountError.innerHTML = finalErrorMessage;
+            focusingElement = bankAccount;
+        }
+        if(isEmptyField(phone.value) || !validatePhonenumber(phone.value)){
+            phoneError.innerHTML = finalErrorMessage;
+            focusingElement = phone;
+        }
+        if(isEmptyField(birthdate.value) || !validateDate(birthdate.value)){
+            birthdateError.innerHTML = finalErrorMessage;
+            focusingElement = birthdate;
+        }
+        if(isEmptyField(dni.value) || !validateDNI(dni.value)){
+            dniError.innerHTML = finalErrorMessage;
+            focusingElement = dni;
+        }
+        if(isEmptyField(mail.value) || !validateMail(mail.value)){
+            mailError.innerHTML = finalErrorMessage;
+            focusingElement = mail;
+        }
+        if(isEmptyField(weight.value) || !validateWeight(weight.value)){
+            weightError.innerHTML = finalErrorMessage;
+            focusingElement = weight;
+        }
+        if(!validateIdiom(idiom.value)){
+            idiomError.innerHTML = finalErrorMessage;
+            focusingElement = idiomCastellano;
+        }
+        if(!validateGender(gender.value)){
+            genderError.innerHTML = finalErrorMessage;
+            focusingElement = genderMale;
+        }
+        if(isEmptyField(surname.value) || !validateSurname(surname.value)){
+            surnameError.innerHTML = finalErrorMessage;
+            focusingElement = surname;
+        }
+        if(isEmptyField(name.value) || !validateName(name.value)){
+            nameError.innerHTML = finalErrorMessage;
+            focusingElement = name;
+        }
+        if(focusingElement===undefined){
+        	document.getElementById("formularioOk").innerHTML = "Validación Completada";
+        }
+        else{
+        	document.getElementById("formularioOk").innerHTML = "Validación Incompleta";
+        	focusingElement.focus();
+        }
     }
 
-    name.addEventListener("blur", function(){
+    name.addEventListener("blur", function(event){
         var valueName = name.value;
+        event.preventDefault();
         if(isEmptyField(valueName)){
             nameError.innerHTML = emptyFieldMessage;
             name.focus();
@@ -181,13 +177,14 @@ window.addEventListener("load", function(){
             nameError.innerHTML = "";
     });
 
-    surname.addEventListener("blur", function(){
+    surname.addEventListener("blur", function(event){
         var valueSurname = surname.value;
+        event.preventDefault();
         if(isEmptyField(valueSurname)){
             surnameError.innerHTML = emptyFieldMessage;
             surname.focus();
         }
-        else if(!validateName(valueSurname)){
+        else if(!validateSurname(valueSurname)){
             surnameError.innerHTML = formatFieldMessage;
             surname.focus();
         }
@@ -320,7 +317,6 @@ window.addEventListener("load", function(){
             urlError.innerHTML = "";
     });
 
-    submit.addEventListener("click", completeValidation);
+    document.forms[0].onsubmit = completeValidation;
+    //submit.addEventListener("click", completeValidation);
 });
-
-
